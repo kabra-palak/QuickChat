5a28f6fd3640b1e22e8747c103187def6157cfb6
kabra-palak <kabrapalak08@gmail.com>
2025-07-26 16:39:00 +0530

    updated for renderr

--- server/server.js
+++ server/server.js
@@ -65,12 +65,12 @@ io.on("connection", (socket) => {
 await connectDB();
 
 // Start server locally (Vercel uses export)
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () =>
-    console.log("Server is running on PORT: " + PORT)
-  );
-}
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server is running on PORT: " + PORT);
+});
+
 
 // Export for Vercel
-export default server;
+// export default server;
